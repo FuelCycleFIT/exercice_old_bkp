b8338f362e1008aa01d95c2951949c3192c14e2f
Nicolas Thiolliere <nicolas.thiolliere@subatech.in2p3.fr>
2017-07-10 13:43:14 -0500

    Update Drawing script

--- EX1/1/Draw.cxx
+++ EX1/1/Draw.cxx
@@ -1,5 +1,4 @@
 {
-
 gStyle->SetOptStat(0);
 
 TFile *FCy = new TFile("cyclus_exo1_1.root");
@@ -7,6 +6,10 @@ TFile *FCl = new TFile("Scenario.root");
 
 TTree *TCy = (TTree *) FCy->Get("TT");
 TTree *TCl = (TTree *) FCl->Get("TreeScenario");
+
+TCy->SetLineColor(kRed); TCy->SetMarkerColor(kRed); TCy->SetLineWidth(3);
+TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
+
 
 TCanvas *C0 = new TCanvas("C0","Pu",1400,900);
 C0->Divide(2,2,0.01,0.01);
@@ -36,11 +39,10 @@ TCy->Draw("B57:T","","Lsame");
 TCl->Draw("B57:T","","Lsame");
 
 C0->cd(4); C0->GetPad(4)->SetPhi(120);
-TH2D *tmp4 = new TH2D("tmp4","Pu 239 in Reactor",10,0,102,10,0,1);
+TH2D *tmp4 = new TH2D("tmp4","Pu 239 in Reactor",10,0,102,10,0,0.6);
 tmp4->GetXaxis()->SetTitle("Time (y)");           tmp4->GetXaxis()->SetTitleOffset(1.0);  tmp4->GetXaxis()->CenterTitle();
 tmp4->GetYaxis()->SetTitle("Mass (tons)");        tmp4->GetYaxis()->SetTitleOffset(1.0);  tmp4->GetYaxis()->CenterTitle();
 tmp4->Draw("");
 TCy->Draw("B170:T","","Lsame");
 TCl->Draw("B170:T","","Lsame");
-
 }

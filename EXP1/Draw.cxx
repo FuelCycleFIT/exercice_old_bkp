d2e413e8abe3d1f48ac2a229c69bb075fe94041f
Nicolas Thiolliere <nicolas.thiolliere@subatech.in2p3.fr>
2017-07-12 12:52:34 -0500

    Update Draw script

--- EXP1/Draw.cxx
+++ EXP1/Draw.cxx
@@ -1,38 +1,71 @@
-void Draw(string s_Exo, string s_Run)
+int Draw(string s_Code1, string s_Code2, string s_Exo1, string s_Exo2, string s_Run1, string s_Run2)
 {
+gROOT->Reset()
 gStyle->SetOptStat(0);
 
-string s_TFile_Cyclus = s_Exo + "/" + s_Run + "/cyclus.root";
-string s_TFile_Class = s_Exo + "/" + s_Run + "/Scenario.root";
+cout<<endl<<"----------------------------------"<<endl;
+cout<<"USAGE : "<<endl;
+cout<<".x Draw.cxx(\"code1\",\"code2\",\"Ex1\",\"Ex2\",\"Run1\",\"Run2\")"<<endl;
+cout<<"EXAMPLE : "<<endl;
+cout<<".x Draw.cxx(\"class\",\"class\",\"EX1\",\"EX1\",\"1\",\"1\")"<<endl;
+cout<<"----------------------------------"<<endl<<endl;
 
-cout<<s_TFile_Cyclus<<endl;
-cout<<s_TFile_Class<<endl;
+string Code1 = s_Code1; string Code2 = s_Code2;
+string Exo1 = s_Exo1; string Exo2 = s_Exo2; 
+string Run1 = s_Run1; string Run2 = s_Run2; 
+string s_TFile_1; string s_TFile_2;
 
+TTree *TC1; TTree *TC2;
 
-TFile *FCy = new TFile(s_TFile_Cyclus.c_str());
-TFile *FCl = new TFile(s_TFile_Class.c_str());
+if (Code1 == "class")        s_TFile_1 = Exo1 + "/" + Run1 + "/Scenario.root";
+else if (Code1 == "cyclus")  s_TFile_1 = Exo1 + "/" + Run1 + "/cyclus.root";
+else
+{
+    cout<<endl<<"code 1 should be \"class\" or \"cyclus\""<<endl<<endl;
+    return 1;
+}
 
-TTree *TCy = (TTree *) FCy->Get("TT");
-TTree *TCl = (TTree *) FCl->Get("TreeScenario");
+if (Code2 == "class") s_TFile_2 = Exo2 + "/" + Run2 + "/Scenario.root";
+else if (Code2 == "cyclus") s_TFile_2 = Exo2 + "/" + Run2 + "/cyclus.root";
+else
+{
+    cout<<endl<<"code 2 should be \"class\" or \"cyclus\""<<endl<<endl;
+    return 1;
+}
 
-TCy->SetLineColor(kRed); TCy->SetMarkerColor(kRed); TCy->SetLineWidth(3);
-TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
+cout<<s_TFile_1<<endl;
+cout<<s_TFile_2<<endl;
+
+TFile *FC1 = new TFile(s_TFile_1.c_str());
+TFile *FC2 = new TFile(s_TFile_2.c_str());
+
+string s_TTreeName_Cyclus = "TT";
+string s_TTreeName_Class = "TreeScenario";
+
+if (Code1 == "cyclus") TC1 = (TTree *) FC1->Get("TT");
+if (Code1 == "class")  TC1 = (TTree *) FC1->Get("TreeScenario");
+
+if (Code2 == "cyclus") TC2 = (TTree *) FC2->Get("TT");
+if (Code2 == "class")  TC2 = (TTree *) FC2->Get("TreeScenario");
+
+TC1->SetLineColor(kRed); TC1->SetMarkerColor(kRed); TC1->SetLineWidth(4);
+TC2->SetLineColor(kBlue); TC2->SetMarkerColor(kBlue); TC2->SetLineWidth(2);
 
 // ################################################################################
 // Legend
 // ################################################################################
     
     TLegend *L01 = new TLegend(0.75,0.10,0.90,0.25);
-    L01->AddEntry(TCy, "Cyclus", "L");
-    L01->AddEntry(TCl, "CLASS", "L");
+    L01->AddEntry(TC1, s_Code1.c_str(), "L");
+    L01->AddEntry(TC2, s_Code2.c_str(), "L");
 
 // ################################################################################
 // ENERGY
 // ################################################################################
 
     TCanvas *C00 = new TCanvas("C00","C00",700,500);
-    TCy->Draw("P:T","","L");
-    TCl->Draw("P:T","","Lsame");
+    TC1->Draw("P:T","","L");
+    TC2->Draw("P:T","","Lsame");
     TH1F *tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Thermal Power");
     tmp0->GetXaxis()->SetTitle("Time (y)");          tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Power (W)");         tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
@@ -47,8 +80,8 @@ TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
     C0->Divide(2,2,0.01,0.01);
         
     C0->cd(1);
-    TCy->Draw("B93/1000:T","","L");
-    TCl->Draw("B93:T","","Lsame");
+    TC1->Draw("B93:T","","L");
+    TC2->Draw("B93:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total Pu");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
@@ -56,8 +89,8 @@ TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
     L01->Draw();
     
     C0->cd(2);
-    TCy->Draw("B3/1000:T","","L");
-    TCl->Draw("B3:T","","Lsame");
+    TC1->Draw("B3:T","","L");
+    TC2->Draw("B3:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Pu in Stock");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
@@ -65,8 +98,8 @@ TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
     L01->Draw();
 
     C0->cd(3);
-    TCy->Draw("B98/1000:T","","L");
-    TCl->Draw("B98:T","","Lsame");
+    TC1->Draw("B98:T","","L");
+    TC2->Draw("B98:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total Pu9");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
@@ -74,9 +107,52 @@ TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
     L01->Draw();
     
     C0->cd(4);
-    TCy->Draw("B8/1000:T","","L");
-    TCl->Draw("B8:T","","Lsame");
+    TC1->Draw("B8:T","","L");
+    TC2->Draw("B8:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Pu9 in Stock");
+    tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
+    tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
+    gPad->Update();
+    L01->Draw();
+
+// ################################################################################
+// U
+// ################################################################################
+
+    TCanvas *C1 = new TCanvas("C1","U",1500,900);
+    C1->Divide(2,2,0.01,0.01);
+        
+    C1->cd(1);
+    TC1->Draw("B102:T","","L");
+    TC2->Draw("B102:T","","Lsame");
+    tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total U5");
+    tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
+    tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
+    gPad->Update();
+    L01->Draw();
+    
+    C1->cd(2);
+    TC1->Draw("B12:T","","L");
+    TC2->Draw("B12:T","","Lsame");
+    tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("U5 in Stock");
+    tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
+    tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
+    gPad->Update();
+    L01->Draw();
+    
+    C1->cd(3);
+    TC1->Draw("B66:T","","L");
+    TC2->Draw("B66:T","","Lsame");
+    tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("U5 in Reactors");
+    tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
+    tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
+    gPad->Update();
+    L01->Draw();
+    
+    C1->cd(4);
+    TC1->Draw("B91:T","","L");
+    TC2->Draw("B91:T","","Lsame");
+    tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total U");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
     gPad->Update();
@@ -86,42 +162,44 @@ TCl->SetLineColor(kBlue); TCl->SetMarkerColor(kBlue); TCl->SetLineWidth(3);
 // MA
 // ################################################################################
 
-    TCanvas *C1 = new TCanvas("C1","MA",1500,900);
-    C1->Divide(2,2,0.01,0.01);
+    TCanvas *C2 = new TCanvas("C2","MA",1500,900);
+    C2->Divide(2,2,0.01,0.01);
         
-    C1->cd(1);
-    TCy->Draw("B96/1000:T","","L");
-    TCl->Draw("B96:T","","Lsame");
+    C2->cd(1);
+    TC1->Draw("B96:T","","L");
+    TC2->Draw("B96:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total MA");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
     gPad->Update();
     L01->Draw();
     
-    C1->cd(2);
-    TCy->Draw("B94/1000:T","","L");
-    TCl->Draw("B94:T","","Lsame");
+    C2->cd(2);
+    TC1->Draw("B94:T","","L");
+    TC2->Draw("B94:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total Am");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
     gPad->Update();
     L01->Draw();
     
-    C1->cd(3);
-    TCy->Draw("B92/1000:T","","L");
-    TCl->Draw("B92:T","","Lsame");
+    C2->cd(3);
+    TC1->Draw("B92:T","","L");
+    TC2->Draw("B92:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total Np");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
     gPad->Update();
     L01->Draw();
     
-    C1->cd(4);
-    TCy->Draw("B95/1000:T","","L");
-    TCl->Draw("B95:T","","Lsame");
+    C2->cd(4);
+    TC1->Draw("B95:T","","L");
+    TC2->Draw("B95:T","","Lsame");
     tmp0 = (TH1F*)gPad->GetPrimitive("htemp"); tmp0->SetTitle("Total Cm");
     tmp0->GetXaxis()->SetTitle("Time (y)");    tmp0->GetXaxis()->CenterTitle();    tmp0->GetXaxis()->SetTitleOffset(0.8);  tmp0->GetXaxis()->SetTitleSize(0.05);
     tmp0->GetYaxis()->SetTitle("Mass (tons)"); tmp0->GetYaxis()->CenterTitle();    tmp0->GetYaxis()->SetTitleOffset(0.8);  tmp0->GetYaxis()->SetTitleSize(0.05);
     gPad->Update();
     L01->Draw();
+
+    return 0;
 }
